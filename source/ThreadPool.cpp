930b2adacdb475101eeadbb53060a3a3db0e1642
xwh <906443532@qq.com>
2026-03-22 16:52:07 +0800

    fix

---
+++ source/ThreadPool.cpp
@@ -0,0 +1,27 @@
+#include "ThreahPool.h"
+
+ThreadPool::ThreadPool(size_t num_threads)
+{
+    for (int i = 0; i < num_threads; ++i) {
+        workers_.emplace_back([this] {
+            while (true) {
+                auto task_opt = task_queue_.wait_and_pop();
+                if (!task_opt.has_value()) {
+                    break;
+                }
+                
+                (*task_opt)();
+            }
+        });
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    task_queue_.stop();
+    for (std::thread& thread : workers_) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+}
