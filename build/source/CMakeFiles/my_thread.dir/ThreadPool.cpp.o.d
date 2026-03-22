cdbaddde7aba431c745fef73d89c4fe932f04a47
xwh <906443532@qq.com>
2026-03-22 16:43:23 +0800

    first commit

--- build/source/CMakeFiles/my_thread.dir/ThreadPool.cpp.o.d
+++
@@ -1,3 +0,0 @@
-source/CMakeFiles/my_thread.dir/ThreadPool.cpp.o: \
- /home/xwh/Project/ProjectA/source/ThreadPool.cpp \
- /usr/include/stdc-predef.h
--- build/test/unit_tests[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( Smoke.Basic /home/xwh/Project/ProjectA/build/test/unit_tests [==[--gtest_filter=Smoke.Basic]==] --gtest_also_run_disabled_tests)
-set_tests_properties( Smoke.Basic PROPERTIES WORKING_DIRECTORY /home/xwh/Project/ProjectA/build/test SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( unit_tests_TESTS Smoke.Basic)
